ed2bee02b73c9f186a351166658079be081c2559
Rhenan Konrad <rhenan_konrad@hotmail.com>
2018-05-23 20:30:55 -0300

    Começo da codificação

---
+++ app/src/main/java/br/ufg/com/dedoduro/ListaDeObras.java
@@ -0,0 +1,15 @@
+package br.ufg.com.dedoduro;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaDeObras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_obras);
+    }
+}
---
+++ app/src/main/java/br/ufg/com/dedoduro/Login.java
@@ -0,0 +1,18 @@
+package br.ufg.com.dedoduro;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
