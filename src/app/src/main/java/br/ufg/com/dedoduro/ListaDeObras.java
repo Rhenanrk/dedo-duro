f93c40c9b4e73e5446fa28f52ace26b176b0833d
Rhenan Konrad <rhenan_konrad@hotmail.com>
2018-05-23 21:48:35 -0300

    Atualização do projeto

--- src/app/src/main/java/br/ufg/com/dedoduro/ListaDeObras.java
+++ src/app/src/main/java/br/ufg/com/dedoduro/ListaDeObras.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ListaDeObras extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class ListaDeObras extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_obras);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
